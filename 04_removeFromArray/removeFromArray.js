9007508d86f84f21a5ce2d0c4c41b46abb5f5d1f
Milton Long <miltonlong17@gmail.com>
2024-08-23 11:15:03 -0400

    added and tested solution to removeFromArray.js file

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    let output = [];
+    array.forEach((item) => {
+        if(!num.includes(item)){
+            output.push(item);
+        }
+    })
+
+    return output;
 };
 
 // Do not edit below this line
