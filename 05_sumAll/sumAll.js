53b14b7ace68922a3ddffd54881a9330235477b5
Milton Long <miltonlong17@gmail.com>
2024-08-23 18:23:24 -0400

    added and tested solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0){
+        return "ERROR";
+    }
+    if(!Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR";
+    }
+    
 
+    let firstTerm = Math.min(start, end);
+    let lastTerm = Math.max(start, end);
+    return lastTerm * (firstTerm + lastTerm) / 2;
 };
 
 // Do not edit below this line
